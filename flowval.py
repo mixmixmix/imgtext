09e259c65135aee54c53881db0d9f6c56d1bb20a
M Kunde <mikolaj.kundegorski@gmail.com>
2021-09-10 16:17:41 +0100

    find peaks

--- flowval.py
+++ flowval.py
@@ -1,6 +1,11 @@
 import pandas as pd
-import seaborne as sns
+import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+import numpy as np
+
+
+
 fdir = pd.read_csv('input/152440flow.csv')
 fval = pd.read_csv('input/152440out.csv')
 
@@ -9,5 +14,19 @@ fval['dtime'] = pd.to_datetime(fval['date'])
 fval = fval[['dtime','flow_v']].set_index(['dtime'])
 fval_sec = fval['flow_v'].resample('S').mean()
 fval_believe = fval_sec.rolling(window=5).mean() #5 second rolling average
-fval_sec.plot()
+x = -1*fval_believe
+peaks, _ = find_peaks(x, height=-4, distance = 4*60) #unit is seconds
+
+plt.plot(x)
+plt.plot(x.iloc[peaks].index, x.iloc[peaks], "x")
+# plt.plot(np.zeros_like(x), "--", color="gray")
 plt.show()
+
+#TODO 1
+#iterate through positions of flow meter to prepare a snapshot of flow profile at given point
+
+#TODO 2
+#iterate through peaks to change sign of the data beyond the peaks
+
+#TODO 3
+#iterate through values of pump change to provide time relative to switch
