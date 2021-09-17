9ea2dee09988cf85bd34e367506b6761c52d3370
M Kunde <mikolaj.kundegorski@gmail.com>
2021-09-17 17:19:49 +0100

    ready for extraction missing some flow meter reading still

--- flowval.py
+++ flowval.py
@@ -9,16 +9,18 @@ import numpy as np
 fdir = pd.read_csv('input/flowdirchange21.csv')
 
 fdir['datetime'] = pd.to_datetime(fdir['datetime'])
+fdir['switch_time'] = fdir['datetime']
 fdir = fdir.set_index(['datetime'])
 
-fval = pd.read_csv('input/152440out.csv')
+fdir = fdir[fdir.index>'23-03-2021 15:24:40']
 
-fval['dtime'] = pd.to_datetime(fval['date'])
+fval = pd.read_csv('input/flowvalchange.csv')
+fval['datetime'] = pd.to_datetime(fval['date'])
 
-fval = fval[['dtime','flow_v']].set_index(['dtime'])
+fval = fval.set_index(['datetime'])
 fval_sec = fval['flow_v'].resample('S').mean()
 fval_believe = fval_sec.rolling(window=5).mean() #5 second rolling average
-x = -1*fval_believe
+x = -1*fval_sec
 peaks, _ = find_peaks(x, height=-4, distance = 4*60) #unit is seconds
 
 plt.plot(x)
@@ -26,8 +28,27 @@ plt.plot(x.iloc[peaks].index, x.iloc[peaks], "x")
 # plt.plot(np.zeros_like(x), "--", color="gray")
 plt.show()
 
+flocs = pd.read_csv('input/flowmeterlocs.csv')
+flocs['datetime'] = pd.to_datetime(flocs['datetime'])
+flocs = flocs.set_index(['datetime'])
+flocs = flocs.sort_index()
+
 #TODO 1
-#iterate through positions of flow meter to prepare a snapshot of flow profile at given point
+#iterate through direction time change to prepare
+# 1 a snapshot of flow profile at given point
+# 2 value of the location
+
+fdl = pd.merge_asof(flocs, fdir, left_index=True, right_index=True)
+
+
+for ind, val in fdir.iterrows():
+    #print(fval[ind])
+    cti = fval_sec.loc[pd.date_range(start=(ind - datetime.timedelta(0,10)),end=(ind+datetime.timedelta(0,150)), freq='1S')]
+    # cti = flocs[ind]
+    print(cti)
+    break
+
+#low flow get 4 min, high flo get 2.5 min
 
 #TODO 2
 #iterate through peaks to change sign of the data beyond the peaks
